df60352b00fa0f86ff2366fe77f552eff5268544
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-03-27 13:01:36 -0500

    feat: :rocket: Ruta post de diaries

--- build/index.js
+++ build/index.js
@@ -4,13 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const diaries_1 = __importDefault(require("./routes/diaries"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json()); // middleware que transforma la req.body a un json
 const PORT = 3000;
 app.get('/ping', (_req, res) => {
-    console.log("Someone ping here!");
-    res.send("Pong");
+    console.log('Someone ping here!');
+    res.send('Pong');
 });
+app.use('/api/diaries', diaries_1.default);
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
