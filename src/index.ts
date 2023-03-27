02ae6e1890799a650ca2b5e8abcd81b6e2004b09
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-03-27 10:56:36 -0500

    feat: :rocket: esLint aplicado

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-import express from 'express';
-import diaryRouter from './routes/diaries';
+import express from 'express'
+import diaryRouter from './routes/diaries'
 
-const app = express();
+const app = express()
 
-app.use(express.json()); // middleware que transforma la req.body a un json
+app.use(express.json()) // middleware que transforma la req.body a un json
 
-const PORT = 3000;
+const PORT = 3000
 
 app.get('/ping', (_req, res) => {
-    console.log("Someone ping here!");
-    res.send("Pong")
-});
+  console.log('Someone ping here!')
+  res.send('Pong')
+})
 
-app.use('/api/diaries', diaryRouter);
+app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-    
-});
+  console.log(`Listening on port ${PORT}`)
+})
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,14 @@
-export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
-export type Visibility = 'poor' | 'moderate' | 'good' | 'excellent';
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
+export type Visibility = 'poor' | 'moderate' | 'good' | 'excellent'
 
 export interface DiaryEntry {
-    id: number,
-    date: string,
-    weather: Weather,
-    visibility: Visibility,
-    comment: string,
+  id: number
+  date: string
+  weather: Weather
+  visibility: Visibility
+  comment: string
 }
+
+// Pick vs Omit
+// export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
